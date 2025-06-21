992b3f16a4e661a061f0ccf4d6854eaa42e57c38
Raissa Machado <raissa.machado@visagio.com>
2025-06-21 12:57:09 -0300

    feat: implement Pessoa class with basic attributes and methods

---
+++ models/Pessoa.java
@@ -0,0 +1,42 @@
+package models;
+
+public abstract class Pessoa {
+    protected String nome;
+    protected String cpf;
+    protected String email;
+
+    public Pessoa(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", CPF: " + cpf + ", Email: " + email;
+    }
+}
