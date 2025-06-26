55fe5be60eebae729fd03a0cd848b30192f550b4
GAN-Gino <143651287+GAN-Gino@users.noreply.github.com>
2025-06-26 10:06:01 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import controllers.*;
-import models.*;
 import gui.*;
+import models.*;
 class Main{
     public static void main(String[] var0){
-        Livro livro1=new Livro("null", "esqueci",2020),livro2=new Livro("A volta dos que num foram", "Jose Chespirito",0),livro3=new Livro("AAAAAAAAAAA", "Eu",1303);
+        Livro livro4=new Livro("nall", "esqueci",2020),livro1=new Livro("noll", "esqueci",2020),livro2=new Livro("A volta dos que num foram", "Jose Chespirito",0),livro3=new Livro("AAAAAAAAAAA", "Eu",1303);
         Aluno aluno1=new Aluno("Eu", "452364","E452364","erererrere"),aluno2=new Aluno("Nao eu", "345654","ererer","iriririr");
         Professor prof1=new Professor("Anastacio", "imecc","kjh@gmail","abafa"),prof2=new Professor("Eugenio", "imecc","grsfvsf@","imecc");
         Biblioteca bib=new Biblioteca();
@@ -11,12 +11,13 @@ class Main{
         bib.cadastrarLivro(livro1);
         bib.cadastrarLivro(livro2);
         bib.cadastrarLivro(livro3);
+        bib.cadastrarLivro(livro4);
         bib.cadastrarUsuario(prof1);
         bib.cadastrarUsuario(prof2);
         bib.cadastrarUsuario(aluno1);
         bib.cadastrarUsuario(aluno2);
-        bib.realizarEmprestimo("null", aluno2);
-        bib.realizarEmprestimo("AAAAAAAAAAA", prof2);
+        bib.realizarEmprestimo("noll", "Eu");
+        bib.realizarEmprestimo("AAAAAAAAAAA", "Anastacio");
         new GUI(bib);
     }
 }
