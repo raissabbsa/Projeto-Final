1f15bb41526fbc9d862b02911c2fb51db667b883
GAN-Gino <143651287+GAN-Gino@users.noreply.github.com>
2025-06-26 13:14:12 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ class Main{
     public static void main(String[] var0){
         Livro livro4=new Livro("nall", "esqueci",2020),livro1=new Livro("noll", "esqueci",2020),livro2=new Livro("A volta dos que num foram", "Jose Chespirito",0),livro3=new Livro("AAAAAAAAAAA", "Eu",1303);
         Aluno aluno1=new Aluno("Eu", "452364","E452364","erererrere"),aluno2=new Aluno("Nao eu", "345654","ererer","iriririr");
-        Professor prof1=new Professor("Anastacio", "imecc","kjh@gmail","abafa"),prof2=new Professor("Eugenio", "imecc","grsfvsf@","imecc");
+        Professor prof1=new Professor("Anastacio", "imecc","kjh@gmail","abafa"),prof2=new Professor("Eugenio", "imekk","grsfvsf@","imecc");
         Biblioteca bib=new Biblioteca();
 
         bib.cadastrarLivro(livro1);
@@ -16,8 +16,6 @@ class Main{
         bib.cadastrarUsuario(prof2);
         bib.cadastrarUsuario(aluno1);
         bib.cadastrarUsuario(aluno2);
-        bib.realizarEmprestimo("noll", "Eu");
-        bib.realizarEmprestimo("AAAAAAAAAAA", "Anastacio");
         new GUI(bib);
     }
 }
---
+++ src/exceptions/LimiteEmprestimosException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class LimiteEmprestimosException extends Exception {
+    public LimiteEmprestimosException(String mensagem) {
+        super(mensagem);
+    }
+}
